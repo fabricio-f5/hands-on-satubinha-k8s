c9e3c4ab322a6d89c3705e6c218a82e707c20d56
fabricio-f5 <fabriciopeloso1@gmail.com>
2026-04-04 19:07:29 +0100

    refact output

--- modules/aws-app-infra/outputs.tf
+++ modules/aws-app-infra/outputs.tf
@@ -3,11 +3,6 @@ output "app_role_arn" {
   value       = aws_iam_role.app.arn
 }
 
-output "oidc_provider_arn" {
-  description = "ARN do OIDC Provider — referência para debugging e outros módulos"
-  value       = aws_iam_openid_connect_provider.this.arn
-}
-
 output "ssm_db_password_path" {
   description = "Path SSM da password do DB — referenciado nos manifests K8s"
   value       = aws_ssm_parameter.db_password.name
